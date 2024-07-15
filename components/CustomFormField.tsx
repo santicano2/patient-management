8eef651b448d875b11669ed1eeefe6ea0e168e11
Santi <canosantiago404@gmail.com>
2024-07-15 19:37:24 -0300

    instalado y agregado textarea

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -20,6 +20,7 @@ import { Select, SelectContent, SelectTrigger, SelectValue } from "./ui/select";
 
 import "react-phone-number-input/style.css";
 import "react-datepicker/dist/react-datepicker.css";
+import { Textarea } from "./ui/textarea";
 
 interface CustomProps {
   control: Control<any>;
@@ -68,6 +69,17 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
             />
           </FormControl>
         </div>
+      );
+    case FormFieldType.TEXTAREA:
+      return (
+        <FormControl>
+          <Textarea
+            placeholder={placeholder}
+            {...field}
+            className="shad-textArea"
+            disabled={props.disabled}
+          />
+        </FormControl>
       );
     case FormFieldType.PHONE_INPUT:
       return (
