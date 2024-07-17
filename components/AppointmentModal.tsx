49200eef2ce851a11c0bbd215b61c15e7aa8dd11
Santi <canosantiago404@gmail.com>
2024-07-17 19:41:45 -0300

    creado appointmentModal

---
+++ components/AppointmentModal.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useState } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "./ui/button";
+
+import AppointmentForm from "./forms/AppointmentForm";
+import { Appointment } from "@/types/appwrite.types";
+
+const AppointmentModal = ({
+  type,
+  patientId,
+  userId,
+  appointment,
+}: {
+  type: "programar" | "cancelar";
+  patientId: string;
+  userId: string;
+  appointment?: Appointment;
+}) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant="ghost"
+          className={`capitalize ${type === "programar" && "text-green-500"}`}
+        >
+          {type}
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="shad-dialog sm:max-w-md">
+        <DialogHeader className="mb-4 space-y-3">
+          <DialogTitle className="capitalize">{type} turno</DialogTitle>
+          <DialogDescription>
+            Porfavor ingrese los siguientes detalles para {type} el turno
+          </DialogDescription>
+        </DialogHeader>
+
+        <AppointmentForm
+          userId={userId}
+          patientId={patientId}
+          type={type}
+          appointment={appointment}
+          setOpen={setOpen}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AppointmentModal;
